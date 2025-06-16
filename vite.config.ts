2c35ad648de40a9aaae99ca4c05fb7986bafaebd
Roman Toporkov <r.toporkov@ya.ru>
2025-06-16 08:09:43 +0300

    chore: update ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     const mockIp = env.VITE_MOCK_IP;
 
     return {
         plugins: [
-            react(),
+            react()
         ],
         resolve: {
             alias: {
-                '@': resolve(__dirname, 'src'),
-            },
+                '@': resolve(__dirname, 'src')
+            }
         },
         define: {
-            __MOCK_IP__: JSON.stringify(mockIp),
+            __MOCK_IP__: JSON.stringify(mockIp)
         },
         css: {
             modules: {
                 localsConvention: 'camelCase',
-                generateScopedName: '[name]__[local]___[hash:base64:5]',
+                generateScopedName: '[name]__[local]___[hash:base64:5]'
             }
         }
-    }
-})
+    };
+});
